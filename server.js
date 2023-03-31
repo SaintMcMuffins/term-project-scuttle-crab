acc60ac5483ea7246951425877223f82331c8828
jmanaois <100259401+jmanaois@users.noreply.github.com>
2023-03-30 19:43:11 -0700

    finish skeleton setup

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-const express = require("express");
 const path = require("path");
 const createError = require("http-errors");
-const requestTime = require("./backend/middleware/request-time");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 
+const express = require("express");
 const app = express();
 
-app.use(requestTime);
-const PORT = process.env.PORT || 3000;
-
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.set("views", path.join(__dirname, "backend", "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "backend", "static")));
-const rootRoutes = require("./backend/routes/root");
 
+const rootRoutes = require("./backend/routes/root");
 app.use("/", rootRoutes);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
 
 app.use((request, response, next) => {
-    next(createError(404));
-  });
+  next(createError(404));
+});
